47ad48e3191b3d1ad15ab4a5a0c20cd6658a5857
Niv Cohen <nivco@payoneer.com>
2018-04-16 01:21:20 +0300

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const HttpStatus = require('http-status-codes');
@@ -10,39 +10,40 @@ const productRoutes = require('./api/routes/products');
 const transactionsRoutes = require('./api/routes/transactions');
 
 
-mongoose.connect('mongodb://node-api:node-api@node-api-shard-00-00-fasjx.mongodb.net:27017,node-api-shard-00-01-fasjx.mongodb.net:27017,node-api-shard-00-02-fasjx.mongodb.net:27017/test?ssl=true&replicaSet=node-api-shard-0&authSource=admin',);
+mongoose.connect('mongodb://node-api:node-api@node-api-shard-00-00-fasjx.mongodb.net:27017,node-api-shard-00-01-fasjx.mongodb.net:27017,node-api-shard-00-02-fasjx.mongodb.net:27017/test?ssl=true&replicaSet=node-api-shard-0&authSource=admin', );
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+//app.use(morgan('dev'));
+app.listen(process.env.PORT || 5000);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use ((req,res,next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method == 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET ,PUT ,POST');  
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET ,PUT ,POST');
         return res.status(HttpStatus.OK).json({});
     }
     next();
 });
 
-app.use('/products',productRoutes);
-app.use('/transactions',transactionsRoutes);
+app.use('/products', productRoutes);
+app.use('/transactions', transactionsRoutes);
 
 
 app.use((req, res, next) => {
-    const error = new Error ('Not Found');
+    const error = new Error('Not Found');
     error.status = HttpStatus.NOT_FOUND;
     next(error);
 });
 
 app.use((error, req, res, next) => {
     res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR)
-    .json({
-        error: {
-            message: error.message
-        }
-    }); 
+        .json({
+            error: {
+                message: error.message
+            }
+        });
 });
 
 module.exports = app;
